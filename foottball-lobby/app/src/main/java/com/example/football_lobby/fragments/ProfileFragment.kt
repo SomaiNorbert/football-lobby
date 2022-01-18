7c9ada8d1f0546c398fe56221d8bf0cf8769468f
SomaiNorbert <somainorbert@yahoo.com>
2022-01-18 23:49:00 +0200

    profil fragment

--- foottball-lobby/app/src/main/java/com/example/football_lobby/fragments/ProfileFragment.kt
+++ foottball-lobby/app/src/main/java/com/example/football_lobby/fragments/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.example.football_lobby.fragments
 
+import android.annotation.SuppressLint
 import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.util.Log
@@ -8,17 +9,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import com.example.football_lobby.R
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 class ProfileFragment : Fragment() {
 
     private lateinit var auth: FirebaseAuth
+
+    private lateinit var profilePic: ImageView
+    private lateinit var name: TextView
+    private lateinit var playedGames: TextView
+    private lateinit var overallRating: TextView
+    private lateinit var email: TextView
+    private lateinit var birthday: TextView
+    private lateinit var aboutMe: TextView
+
+
+    val db = Firebase.firestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,15 +48,55 @@ class ProfileFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val txt = view.findViewById<TextView>(R.id.txt)
         val user = auth.currentUser
-        txt.text = user!!.email
+        if(user == null){
+            navigateToStartScreen()
+        }else{
 
-        view.findViewById<Button>(R.id.logOutBtn).setOnClickListener {
-            Firebase.auth.signOut()
-            findNavController().navigate(R.id.action_profileFragment_to_startFragment)
+            profilePic = view.findViewById(R.id.profilePictureImageView)
+            name = view.findViewById(R.id.playerNameTxt)
+            playedGames = view.findViewById(R.id.playedGamesTxt)
+            overallRating = view.findViewById(R.id.ratingTxt)
+            email = view.findViewById(R.id.emailTxt)
+            birthday = view.findViewById(R.id.birthdayTxt)
+            aboutMe = view.findViewById(R.id.aboutMeTxt)
+
+            db.collection("users").whereEqualTo("uid", user.uid).get()
+                .addOnSuccessListener { result ->
+                    val userData = result.documents[0]
+                    Glide.with(this).load(userData["profilePic"]).into(profilePic)
+                    name.text = userData["name"].toString()
+                    playedGames.text = userData["numberOfGamesPlayed"].toString()
+                    if(userData["overallRating"].toString() == "0"){
+                        overallRating.text = "-"
+                    }else{
+                        overallRating.text = userData["overallRating"].toString() + "/10"
+                    }
+                    email.text = userData["email"].toString()
+                    birthday.text = userData["birthday"].toString()
+                    aboutMe.text = userData["aboutMe"].toString()
+            }
+
+            view.findViewById<Button>(R.id.logOutBtn).setOnClickListener {
+                auth.signOut()
+                navigateToStartScreen()
+            }
+
+            view.findViewById<Button>(R.id.deleteProfileBtn).setOnClickListener{
+                user.delete();
+                navigateToStartScreen()
+            }
+
+            view.findViewById<Button>(R.id.editProfileBtn).setOnClickListener {
+                TODO("Not implemented yet!")
+            }
         }
+    }
+
+    private fun navigateToStartScreen(){
+        findNavController().navigate(R.id.action_profileFragment_to_startFragment)
     }
 }
