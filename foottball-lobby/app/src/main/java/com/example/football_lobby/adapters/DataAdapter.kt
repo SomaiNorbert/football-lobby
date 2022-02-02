be5884d20e4e42e1bd74ab2776152090dabb2d97
SomaiNorbert <somainorbert@yahoo.com>
2022-02-02 15:25:24 +0100

    filtering feature added

--- foottball-lobby/app/src/main/java/com/example/football_lobby/adapters/DataAdapter.kt
+++ foottball-lobby/app/src/main/java/com/example/football_lobby/adapters/DataAdapter.kt
@@ -1,20 +1,40 @@
 package com.example.football_lobby.adapters
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.football_lobby.R
 import com.example.football_lobby.models.Lobby
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.tasks.Tasks
+import java.util.*
+import kotlin.collections.ArrayList
 
 class DataAdapter(
-    private var list: List<Lobby>,
+    private var list: ArrayList<Lobby>,
     private var listener: OnItemClickedListener
-    ) : RecyclerView.Adapter<DataAdapter.RecyclerViewHolder>(){
+    ) : RecyclerView.Adapter<DataAdapter.RecyclerViewHolder>(), Filterable{
+
+    private val listFull = ArrayList<Lobby>()
+    private var context: Context? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewHolder {
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.lobby_item_layout, parent, false)
+        context = parent.context
         return RecyclerViewHolder(itemView)
     }
 
@@ -43,6 +63,9 @@ class DataAdapter(
 
         init{
             itemView.setOnClickListener(this)
+            if(listFull.isEmpty()){
+                listFull.addAll(list)
+            }
         }
 
         override fun onClick(p0: View?) {
@@ -51,11 +74,93 @@ class DataAdapter(
                 listener.onItemClick(position)
             }
         }
+
     }
 
     interface OnItemClickedListener{
         fun onItemClick(position:Int)
     }
 
+    fun setData(list:ArrayList<Lobby>){
+        this.list = list
+    }
+
+    override fun getFilter(): Filter {
+        return searchFilter
+    }
+
+    private val searchFilter = object : Filter(){
+        @SuppressLint("MissingPermission")
+        override fun performFiltering(p0: CharSequence?): FilterResults {
+            val results = FilterResults()
+            if(p0 == null){
+                results.values = list
+                return results
+            }
+
+            val filters: List<String> = p0.split("/")
+            val filteredByNameAndCreator = ArrayList<Lobby>()
+            if(filters[0].isEmpty() && filters[1].isEmpty()){  //Name and Creator is Empty: filtered list is equal to full list
+                filteredByNameAndCreator.addAll(listFull)
+            }else{
+                if(filters[0].isEmpty()){   //Name is Empty: we filter the full list by Creator
+                    for(lobby in 0 until listFull.size){
+                        if(listFull[lobby].creator.lowercase().trim().contains(filters[1].lowercase().trim())){
+                            filteredByNameAndCreator.add(listFull[lobby])
+                        }
+                    }
+                }else{
+                    if(filters[1].isEmpty()){    //Creator is Empty: we filter the full list by Name
+                        for(lobby in 0 until listFull.size){
+                            if(listFull[lobby].lobbyName.lowercase().trim().contains(filters[0].lowercase().trim())){
+                                filteredByNameAndCreator.add(listFull[lobby])
+                            }
+                        }
+                    }else{
+                        for(lobby in 0 until listFull.size){   //Neither Name nor creator is empty: we filter by both
+                            if(listFull[lobby].lobbyName.lowercase().trim().contains(filters[0].lowercase().trim()) &&
+                                listFull[lobby].creator.lowercase().trim().contains(filters[1].lowercase().trim())){
+                                filteredByNameAndCreator.add(listFull[lobby])
+                            }
+                        }
+                    }
+                }
+            }
+            //filter the filtered list by distance
+            val filteredByAll = ArrayList<Lobby>()
+
+            val geocoder = Geocoder(context, Locale.getDefault())
+            if(ContextCompat.checkSelfPermission(context!!, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+                    val location = Tasks.await(LocationServices.getFusedLocationProviderClient(context!!).lastLocation);
+                    val myLocation = LatLng(location.latitude, location.longitude)
+                    for (lobby in 0 until filteredByNameAndCreator.size) {
+                        val add = geocoder.getFromLocationName(
+                            filteredByNameAndCreator[lobby].location,
+                            1
+                        )[0]
+                        val gameLocation = LatLng(add.latitude, add.longitude)
+                        val result = FloatArray(3)
+                        Location.distanceBetween(
+                            myLocation.latitude, myLocation.longitude,
+                            gameLocation.latitude, gameLocation.longitude, result
+                        )
+                        if (result[0] / 1000 <= filters[2].toInt()) {
+                            filteredByAll.add(filteredByNameAndCreator[lobby])
+                        }
+                    }
+            }
+            results.values = filteredByAll
+            return results
+        }
+
+        override fun publishResults(p0: CharSequence?, p1: FilterResults?) {
+            list.clear()
+            if(p1?.values != null)
+                list.addAll(p1.values as ArrayList<Lobby>)
+            notifyDataSetChanged()
+        }
+
+    }
 
 }
--- foottball-lobby/app/src/main/java/com/example/football_lobby/fragments/FindLobbyFragment.kt
+++ foottball-lobby/app/src/main/java/com/example/football_lobby/fragments/FindLobbyFragment.kt
@@ -1,11 +1,6 @@
 package com.example.football_lobby.fragments
 
-import android.Manifest
-import android.annotation.SuppressLint
 import android.content.ContentValues.TAG
-import android.content.pm.PackageManager
-import android.location.Geocoder
-import android.location.Location
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -14,28 +9,23 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.TextView
-import androidx.core.content.ContextCompat
 import androidx.core.widget.doOnTextChanged
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.football_lobby.R
 import com.example.football_lobby.adapters.DataAdapter
 import com.example.football_lobby.models.Lobby
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.maps.model.LatLng
 import com.google.android.material.slider.Slider
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import kotlinx.coroutines.awaitAll
-import java.lang.Thread.sleep
-import java.math.BigDecimal
-import java.math.RoundingMode
+import com.google.android.gms.tasks.Tasks
 import java.util.*
 import kotlin.collections.ArrayList
 
 class FindLobbyFragment : Fragment(), DataAdapter.OnItemClickedListener {
 
+    private lateinit var adapter: DataAdapter
     private lateinit var db: FirebaseFirestore
     private lateinit var foundLobbiesRecyclerView: RecyclerView
     private lateinit var findLobbyByName: EditText
@@ -65,17 +55,17 @@ class FindLobbyFragment : Fragment(), DataAdapter.OnItemClickedListener {
         foundLobbiesRecyclerView = view.findViewById(R.id.foundLobbiesRecyclerView)
         distanceSlider = view.findViewById(R.id.distanceSlider)
         val distTxt = view.findViewById<TextView>(R.id.distanceTxt)
+        setupRecyclerView()
+        loadAllLobbiesIntoAdapter()
 
         distanceSlider.addOnChangeListener { _, value, _ ->
-            if(value < 1){
+            if (value < 1) {
                 distTxt.text = "<1"
-            }else{
+            } else {
                 distTxt.text = value.toInt().toString()
             }
-            filterByDistance()
+            filter()
         }
-
-        getAllLobbies()
 
         findLobbyByName.doOnTextChanged { _,_,_,_ ->
             filter()
@@ -83,70 +73,15 @@ class FindLobbyFragment : Fragment(), DataAdapter.OnItemClickedListener {
         findLobbyByCreator.doOnTextChanged { _, _, _, _ ->
             filter()
         }
-
+        //filter() TODO
     }
 
-    private fun filter(){
-        listCurrent.clear()
-        if(findLobbyByName.text.isEmpty() && findLobbyByCreator.text.isEmpty() ||
-                findLobbyByCreator.text == null && findLobbyByName.text == null){
-            listCurrent.addAll(listFull)
-        }else{
-            if(findLobbyByName.text.isEmpty()){
-                for(lobby in 0 until listFull.size){
-                    if(listFull[lobby].creator.lowercase().trim().contains(findLobbyByCreator.text.toString().lowercase().trim())){
-                        listCurrent.add(listFull[lobby])
-                    }
-                }
-            }else{
-                if(findLobbyByCreator.text.isEmpty()){
-                    for(lobby in 0 until listFull.size){
-                        if(listFull[lobby].lobbyName.lowercase().trim().contains(findLobbyByName.text.toString().lowercase().trim())){
-                            listCurrent.add(listFull[lobby])
-                        }
-                    }
-                }else{
-                    for(lobby in 0 until listFull.size){
-                        if(listFull[lobby].lobbyName.lowercase().trim().contains(findLobbyByName.text.toString().lowercase().trim()) &&
-                            listFull[lobby].creator.lowercase().trim().contains(findLobbyByCreator.text.toString().lowercase().trim())){
-                            listCurrent.add(listFull[lobby])
-                        }
-                    }
-                }
-            }
-        }
-
-        showLobbies(listCurrent)
-        filterByDistance()
+    private fun filter() {
+        adapter.filter.filter(findLobbyByName.text.toString() + "/" +
+                findLobbyByCreator.text.toString() + "/" + distanceSlider.value.toInt().toString())
     }
 
-    @SuppressLint("MissingPermission")
-    private fun filterByDistance(){
-        val list2 = ArrayList<Lobby>()
-        val geocoder = Geocoder(context, Locale.getDefault())
-        if(ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
-            == PackageManager.PERMISSION_GRANTED) {
-            LocationServices.getFusedLocationProviderClient(requireContext()).lastLocation.addOnSuccessListener {
-                val myLocation = LatLng(it.latitude, it.longitude)
-                for(lobby in 0 until listCurrent.size){
-                    val add = geocoder.getFromLocationName(listCurrent[lobby].location, 1)[0]
-                    val gameLocation = LatLng(add.latitude, add.longitude)
-                    val results = FloatArray(3)
-                    Location.distanceBetween(myLocation.latitude, myLocation.longitude,
-                        gameLocation.latitude, gameLocation.longitude, results)
-                    Log.d(TAG, "Dist between My location and ${listCurrent[lobby].location}: ${results[0]/1000}")
-                    Log.d(TAG, distanceSlider.value.toString())
-                    if(results[0]/1000 <= distanceSlider.value){
-                        list2.add(listCurrent[lobby])
-                        Log.d(TAG, "ASD")
-                    }
-                }
-                showLobbies(list2)
-            }
-        }
-    }
-
-    private fun getAllLobbies(){
+    private fun loadAllLobbiesIntoAdapter(){
         val list = ArrayList<Lobby>()
         db.collection("lobbies").get().addOnSuccessListener {
             result ->
@@ -155,13 +90,14 @@ class FindLobbyFragment : Fragment(), DataAdapter.OnItemClickedListener {
                 lobby["time"].toString(), lobby["createdBy"].toString(), lobby["numberOfPlayersInLobby"].toString().toInt(),
                 lobby["maximumNumberOfPlayers"].toString().toInt(), lobby["public"] as Boolean))
             }
-            listFull = list
-            filter()
+            adapter.setData(list)
+            adapter.notifyDataSetChanged()
         }
     }
 
-    private fun showLobbies(list:ArrayList<Lobby>){
-        foundLobbiesRecyclerView.adapter = DataAdapter(list, this)
+    private fun setupRecyclerView(){
+        adapter = DataAdapter(ArrayList<Lobby>(), this)
+        foundLobbiesRecyclerView.adapter = adapter
         foundLobbiesRecyclerView.layoutManager = LinearLayoutManager(requireContext())
         foundLobbiesRecyclerView.setHasFixedSize(true)
     }
